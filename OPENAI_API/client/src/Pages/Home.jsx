551295b4c962b4ed663c90fc29e0b9f4592e2c44
Kossay-Zemzem <kossayzemzem@gmail.com>
2024-06-18 12:23:45 +0100

    Started layout of Home page
    Added two divs for home age

--- OPENAI_API/client/src/Pages/Home.jsx
+++ OPENAI_API/client/src/Pages/Home.jsx
@@ -11,22 +11,65 @@ function Home() {
         mix-blend-overlay opacity-50"
         />
       </header>
+
+      <div className="h-full flex align-middle justify-center ">
+        <div
+          className="bg-black bg-opacity-35 rounded-lg
+        ml-4 mr-4 pr-8 pl-8 pt-3 pb-3
+        "
+        >
+          <div
+            className="text-center text-white
+            mb-5
+          
+          "
+          >
+            إنشاء حساب جديد
+          </div>
+          <div className="mb-5">
+            <input
+              placeholder="الأسم"
+              type="text"
+              id="prenom"
+              className="bg-[#D9D9D9]
+              border border-red-600 rounded-lg
+              block w-full p-1.5
+             text-black text-xl text-center
+             placeholder-gray-400 placeholder:text-center 
+             focus:ring-2             
+             focus:ring-[#057933]
+             hover:scale-110 ease-in-out duration-300
+             "
+              required
+            />
+          </div>
+        </div>
+        <div
+          className="bg-black bg-opacity-35
+        ml-4 mr-4 pr-8 pl-8 pt-3 pb-3
+        "
+        >
+          <div className="mb-5">
+            <input
+              placeholder="الأسم"
+              type="text"
+              id="prenom"
+              className="bg-[#D9D9D9]
+              border border-red-600 rounded-lg
+              block w-full p-1.5
+             text-black text-xl text-center
+             placeholder-gray-400 placeholder:text-center 
+             focus:ring-2             
+             focus:ring-[#057933]
+             hover:scale-110 ease-in-out duration-300
+             "
+              required
+            />
+          </div>
+        </div>
+      </div>
       <div>
         <Link to="CIN">NEXT</Link>
-      </div>
-      <div className="h-full flex items-center align-middle">
-        <div
-          className="bg-black 
-        text-white"
-        >
-          BOX1
-        </div>
-        <div
-          className="bg-black 
-        text-white"
-        >
-          BOX2
-        </div>
       </div>
     </div>
   );
