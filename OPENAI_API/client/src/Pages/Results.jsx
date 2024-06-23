5e533a0dd5ce999b4b8a48ce16f6ba9eb6545934
Kossay-Zemzem <kossayzemzem@gmail.com>
2024-06-23 12:12:34 +0100

    Made first document card (initial layout only)
    Still need to style things better + add a dynmaic image depending on the progress percentage

--- OPENAI_API/client/src/Pages/Results.jsx
+++ OPENAI_API/client/src/Pages/Results.jsx
@@ -15,9 +15,12 @@ function Results() {
         />
       </header>
       <div className="flex justify-around">
-        <div className="w-80">
+        <div className="flex flex-col items-center w-80">
+          <div className="h-44 w-full mb-3 border-2 border-gray-100 text-white text-center">
+            CIN animation placeholder
+          </div>
           <div
-            className=" bg-gray-200 h-4 rounded-full"
+            className=" bg-gray-200 h-4 rounded-full w-full"
             style={{ direction: "ltr" }}
           >
             <div
@@ -31,6 +34,29 @@ function Results() {
           >
             تواقف بنسبة {progressCIN} %
           </div>
+          <button
+            type="submit"
+            className="
+                bg-gradient-to-b from-[#FED33D] from-15% to-[#F67C0B]
+                rounded-lg
+                text-black text-lg 
+                font-medium 
+                sm:w-20
+                px-3 py-2
+                my-2 
+                text-center
+                hover:ease-in-out duration-500
+                hover:bg-opacity-50
+                
+                hover:text-white  
+                hover:ring-zinc-300 
+                hover:ring-2 
+                hover:outline-none
+  
+                "
+          >
+            <Link to="/">التفاصيل</Link>
+          </button>
         </div>
       </div>
     </div>
